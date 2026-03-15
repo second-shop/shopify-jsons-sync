dd5ddb216f88cc632fa5c0a4147eec748b40561b
Meder Toktosunov <m.toktosunov@icloud.com>
2026-03-14 21:11:33 -0400

    Update Node.js version to 24.x and adjust related dependencies in package.json and package-lock.json

--- dist/index.js
+++ dist/index.js
@@ -22,13 +22,23 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
 const utils_1 = __nccwpck_require__(918);
@@ -102,7 +112,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.getNewTemplatesToRemote = exports.syncLocaleAndSettingsJSON = exports.removeDisabledKeys = exports.sendFilesWithPathToShopify = exports.validateShopifyCliAccess = exports.execShellCommand = exports.cleanRemoteFiles = exports.readJsonFile = exports.fetchFiles = exports.EXEC_OPTIONS = void 0;
+exports.getNewTemplatesToRemote = exports.syncLocaleAndSettingsJSON = exports.removeDisabledKeys = exports.sendFilesWithPathToShopify = exports.validateShopifyCliAccess = exports.cleanRemoteFiles = exports.readJsonFile = exports.fetchFiles = exports.EXEC_OPTIONS = void 0;
+exports.execShellCommand = execShellCommand;
 const glob_1 = __nccwpck_require__(8090);
 const promises_1 = __nccwpck_require__(3292);
 const fs_1 = __nccwpck_require__(7147);
@@ -192,7 +203,6 @@ async function execShellCommand(cmd) {
         });
     });
 }
-exports.execShellCommand = execShellCommand;
 const validateShopifyCliAccess = async (store, execCommand = execShellCommand) => {
     try {
         await execCommand('shopify version');
